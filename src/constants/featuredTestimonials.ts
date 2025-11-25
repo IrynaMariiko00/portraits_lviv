0dd09dbaab7da8961f49e9ff3e7dc0f1442a74dc
Iryna Mariiko <dotcodeteam@flyjets.com>
2025-11-25 20:02:53 +0200

    fix images

--- src/constants/featuredTestimonials.ts
+++ src/constants/featuredTestimonials.ts
@@ -1,4 +1,4 @@
 export const featuredTestimonials: string[] = [];
 
-for (let i = 1; i < 8; ++i)
-  featuredTestimonials.push(`./images/featuredPortraits/${i}.jpg`);
+for (let i = 1; i < 5; ++i)
+  featuredTestimonials.push(`./images/featuredTestimonials/${i}.png`);
